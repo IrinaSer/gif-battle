997960ad753cb7cda468ae9cc4e9bbaaebde1b36
IrinaSer <celebro13@mail.ru>
2019-05-13 20:17:39 +0300

    start module

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
+const gif = require('./lib/giphy.js');
 http.createServer((req, res) => {
-  console.log(req.url);
-  console.log(req.method);
   res.writeHead(200, {'Content-type': 'application/json'});
+
   switch (req.url) {
     case '/':
       if (req.method === 'GET') {
@@ -10,19 +10,27 @@ http.createServer((req, res) => {
       } else if (req.method === 'POST') {
         res.end('creating');
       }
+      break;
     case '/random':
+      const url = 'http://api.giphy.com/v1/gifs/random?api_key=3VNNco8hjxksb4zlL4vMfu12uE5VhLK1';
+      gif.get(url, (error, gif) => {
+        if (error) throw error;
+
+        console.log(gif);
+      });
       res.end('getting a random');
+      break;
     case '/versus':
       res.end('getting versus');
+      break;
     case '/vote':
       if (req.method === 'POST') {
         res.end('voting');
       }
+      break;
     default:
       res.writeHead(404, {'Content-type': 'application/json'});
-      res.end('404');
+      res.end('404 not found');
   }
-
-
 
 }).listen(3000, () => console.log('Server works!'));
